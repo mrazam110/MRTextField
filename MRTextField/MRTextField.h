ced8487512107f1c2a4e2c0ab85adf5efbbbba15
mrazam110 <mrazam110@gmail.com>
2015-12-24 19:49:56 +0500

    Initial Commit

---
+++ MRTextField/MRTextField.h
@@ -0,0 +1,19 @@
+//
+//  MRTextField.h
+//  MRTextField
+//
+//  Created by Muhammad Raza on 24/12/2015.
+//  Copyright © 2015 Muhammad Raza Master. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRTextField.
+FOUNDATION_EXPORT double MRTextFieldVersionNumber;
+
+//! Project version string for MRTextField.
+FOUNDATION_EXPORT const unsigned char MRTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRTextField/PublicHeader.h>
+
+
